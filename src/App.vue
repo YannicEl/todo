27b89771eb3299ff6ed8d87cf26db722645a8464
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2021-05-08 15:45:05 +0200

    added temp logo

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+  <Nav />
   <router-view />
 </template>
 
---
+++ src/components/Nav.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="container my-4">
+    <img src="../assets/logo.svg" alt="logo" class="h-12"/>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
