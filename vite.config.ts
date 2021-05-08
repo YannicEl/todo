b0f47c7611fd5438f52a870d8f8845f47070950e
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2021-05-08 22:41:08 +0200

    moved pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,30 @@ import ViteComponents from 'vite-plugin-components';
 import WindiCSS from 'vite-plugin-windicss';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const manifest = {
+  name: 'ToDo',
+  short_name: 'ToDo',
+  start_url: 'https://todo.yannic.tf/',
+  display: 'standalone',
+  background_color: '#ffffff',
+  description:
+    'A simple ToDo app I made for myself but I guess everybody can use it.',
+  icons: [
+    {
+      src: './icons/icon-144x144.png',
+      sizes: '144x144',
+      type: 'image/png',
+    },
+  ],
+};
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), ViteComponents(), WindiCSS(), VitePWA()],
+  plugins: [
+    vue(),
+    ViteComponents(),
+    WindiCSS(),
+    VitePWA({
+      manifest,
+    }),
+  ],
 });
