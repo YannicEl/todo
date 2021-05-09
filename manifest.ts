6beea19faf7ccf799a14d50ffb50bb9ffdfea76d
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2021-05-09 09:54:12 +0200

    added pwa icons

---
+++ manifest.ts
@@ -0,0 +1,36 @@
+export const manifest = {
+  name: 'ToDo',
+  short_name: 'ToDo',
+  start_url: 'https://todo.yannic.tf/',
+  display: 'standalone',
+  background_color: '#ffffff',
+  description:
+    'A simple ToDo app I made for myself but I guess everybody can use it.',
+  icons: [
+    {
+      src: './icons/icon-144x144.png',
+      sizes: '144x144',
+      type: 'image/png',
+    },
+    {
+      src: './icons/icon-192x192.png',
+      sizes: '192x192',
+      type: 'image/png',
+    },
+    {
+      src: './icons/icon-144x144.png',
+      sizes: '144x144',
+      type: 'image/png',
+    },
+    {
+      src: './icons/icon-256x256.png',
+      sizes: '256x256',
+      type: 'image/png',
+    },
+    {
+      src: './icons/icon-512x512.png',
+      sizes: '512x512',
+      type: 'image/png',
+    },
+  ],
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,23 +3,8 @@ import vue from '@vitejs/plugin-vue';
 import ViteComponents from 'vite-plugin-components';
 import WindiCSS from 'vite-plugin-windicss';
 import { VitePWA } from 'vite-plugin-pwa';
+import { manifest } from './manifest';
 
-const manifest = {
-  name: 'ToDo',
-  short_name: 'ToDo',
-  start_url: 'https://todo.yannic.tf/',
-  display: 'standalone',
-  background_color: '#ffffff',
-  description:
-    'A simple ToDo app I made for myself but I guess everybody can use it.',
-  icons: [
-    {
-      src: './icons/icon-144x144.png',
-      sizes: '144x144',
-      type: 'image/png',
-    },
-  ],
-};
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
