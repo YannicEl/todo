ff7577ba8a2c2ef0b51778637255ed5010638239
yannic.ellhotka@gmail.com <yannic.ellhotka@gmail.com>
2021-05-09 21:37:59 +0200

    added vuex persistent store plugin

--- src/state/store.ts
+++ src/state/store.ts
@@ -2,24 +2,15 @@ import { InjectionKey } from 'vue';
 import { createStore, Store, useStore as baseUseStore } from 'vuex';
 import { State } from './State';
 import { mutations } from './mutations';
+import createPersistedState from 'vuex-persistedstate';
+import { createLogger } from 'vuex';
 
 export const key: InjectionKey<Store<State>> = Symbol();
 
 export const store = createStore<State>({
+  plugins: [createPersistedState(), createLogger()],
   state: {
-    tasks: [
-      {
-        id: '1',
-        days: 123,
-        subtasks: [
-          { id: '3bjvzfgVedrJ3dLwGeqvE', completed: true, label: 'Task 1' },
-          { id: 'ZdHbjwIgRC7iuM4m5ZsyV', completed: false, label: 'Task 2' },
-          { id: 'MazQa0nsjhlC3YC_gNdI4', completed: true, label: 'Task 3' },
-          { id: '74FpdRk34kSGnK9tF527Y', completed: false, label: 'Task 4' },
-          { id: 'mJ5FHShUWfMPS5kIqiqwC', completed: true, label: 'Task 5' },
-        ],
-      },
-    ],
+    tasks: [],
   },
   mutations,
 });
